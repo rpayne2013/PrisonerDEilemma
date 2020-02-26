a1f5cd339718fe8650a0daf69f7a9f9b949c87cd
urmom <61281042+623764@users.noreply.github.com>
2020-02-26 08:02:55 -0600

    Update team1edidif.py

--- team1edidif.py
+++ team1edidif.py
@@ -9,6 +9,9 @@
 big_fingas = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
+teaam_name = 'big fingas' # Only 10 chars displayed.
+strategy_name = 'kanye west he likes big fingas'
+strategy_description = 'whoever has the biggest fingas dies'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
